2679bc326c8fad83e3affe017a539a36a5eec8c4
Tim Tesner <ttesner+twitter@gmail.com>
2025-01-21 15:07:10 -0500

    Update LinkLore.js
    experimental address bar color changer

--- LinkLore.js
+++ LinkLore.js
@@ -318,4 +318,18 @@ function readInput() {
     say(_.sample(empty_talk));
   }
 }
+
+//__________________________________________
+function AddressBarColorFlasher(){
+  function addressBarColor(){
+    let rndColorSetter=setInterval(
+    ()=>{
+      function meta_rnd_clr()=>{return "#" + Math.floor(Math.random() * 16777215).toString(16);}
+      let meta_id=document.getElementById('meta_id');
+      meta_id.setAttribute('content', meta_rnd_clr);
+    },100)
+}
+setTimeout(clearInterval(rndColorSetter),5000);
+}
+}
 /*--To Do List/Fix/Redo--*/
