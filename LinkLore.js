16da5027b20ae3469a2c209c016ad2aebfe49936
Tim Tesner <ttesner+twitter@gmail.com>
2025-01-21 15:39:48 -0500

    Update LinkLore.js
    addressBarColor edit

--- LinkLore.js
+++ LinkLore.js
@@ -329,9 +329,10 @@ function AddressBarColorFlasher(){
       function meta_rnd_clr()=>{return "#" + Math.floor(Math.random() * 16777215).toString(16);}
       let meta_id=document.getElementById('meta_id');
       meta_id.setAttribute('content', ' + meta_rnd_clr() + ');
-    },100)
+    },100);
+    addressBarColor();
 }
 setTimeout(clearInterval(rndColorSetter),5000);
 }
-}
+//}
 /*--To Do List/Fix/Redo--*/
