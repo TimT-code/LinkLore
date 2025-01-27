931806972a981ea42127eab5e3a2821a12a45337
Tim Tesner <ttesner+twitter@gmail.com>
2025-01-26 20:37:37 -0500

    Update LinkLore.js

--- LinkLore.js
+++ LinkLore.js
@@ -127,10 +127,10 @@ function favicons_and_links_lister(){
     const theImg = document.getElementById('link_' + i + '_id');
     const theImg_w = theImg.width;
     const theImg_h = theImg.height;
-    if(theImg_w < 32){
-      theImg.width='64';
-      theImg.height='64':
-    };
+    /*if(theImg_w < 32){
+      theImg.width='64px';
+      theImg.height='64px':
+    };*/
     IDx('theLink').value = '';
     alert(theImg.width);
   }
